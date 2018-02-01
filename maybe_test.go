314b5f8e36286e31ec4938ff36117bd6e1745bf9
Franco Lombardo <f dot lombardo 69 at gmail dot com>
2018-02-01 22:17:38 +0100

    Added new test for OrElse

--- maybe_test.go
+++ maybe_test.go
@@ -7,11 +7,22 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestOrElse(t *testing.T) {
+func TestOrElseCallFunctionWithErrorMaybe(t *testing.T) {
 	f := func(_ interface{}) (interface{}, error) {
 		return "OK", nil
 	}
 	m := NewMayBe(nil, errors.New("First error"))
 	m1 := m.OrElse(nil, f)
 	assert.Nil(t, m1.err)
+	assert.Equal(t, "OK", m1.value)
+}
+
+func TestOrElseDoesntCallFunctionWithSomeMaybe(t *testing.T) {
+	f := func(_ interface{}) (interface{}, error) {
+		return "OK", nil
+	}
+	m := NewMayBe("Original", nil)
+	m1 := m.OrElse(nil, f)
+	assert.Nil(t, m1.err)
+	assert.Equal(t, "Original", m1.value)
 }
