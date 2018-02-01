39e220eb01afd113025cc382b0aec0cc97747220
Franco Lombardo <f dot lombardo 69 at gmail dot com>
2018-02-01 22:14:01 +0100

    OrElse: first version

--- maybe.go
+++ maybe.go
@@ -1,19 +1,19 @@
 package maybe
 
-type MayBeFunction func() (interface{}, error)
+type MayBeFunction func(x interface{}) (interface{}, error)
 
 type MayBe struct {
 	value interface{}
 	err   error
 }
 
-func NewMayBe(i interface{}, e error) *MayBe {
-	return &MayBe{i, e}
+func NewMayBe(value interface{}, err error) *MayBe {
+	return &MayBe{value, err}
 }
 
-func (m *MayBe) OrElse(f MayBeFunction) *MayBe {
-	if m.err == nil {
-		return NewMayBe(f())
+func (m *MayBe) OrElse(x interface{}, f MayBeFunction) *MayBe {
+	if m.err != nil {
+		return NewMayBe(f(x))
 	}
 	return m
 }
--- maybe_test.go
+++ maybe_test.go
@@ -3,14 +3,15 @@ package maybe
 import (
 	"errors"
 	"testing"
+
 	"github.com/stretchr/testify/assert"
 )
 
 func TestOrElse(t *testing.T) {
-	f := func(_ onterface{}) (interface{}, error) {
+	f := func(_ interface{}) (interface{}, error) {
 		return "OK", nil
 	}
 	m := NewMayBe(nil, errors.New("First error"))
-	m1 := m.OrElse(f)
-	assert.Nil(m1.err)
+	m1 := m.OrElse(nil, f)
+	assert.Nil(t, m1.err)
 }
